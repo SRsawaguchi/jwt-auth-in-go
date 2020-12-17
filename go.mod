ae66d9cb95169a9e148b41e4e10a4b934955d4eb
Naoya Sawaguchi <naoyasawaguchi978@gmail.com>
2020-12-17 11:35:13 +0900

    Added JWT utility functions

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/SRsawaguchi/simple-jwt-auth-in-go
 
 go 1.14
+
+require github.com/dgrijalva/jwt-go v3.2.0+incompatible
---
+++ internal/auth/jwt.go
@@ -0,0 +1,47 @@
+package auth
+
+import (
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+// NamedClaims represents payload of JWT token
+type NamedClaims struct {
+	Name string `json:"name"`
+	jwt.StandardClaims
+}
+
+// GenerateToken generates JWT token with passed secretV
+func GenerateToken(username, secret string, expireDuration time.Duration) (string, error) {
+	claim := NamedClaims{
+		Name: username,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(expireDuration).Unix(),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claim)
+	tokenString, err := token.SignedString([]byte(secret))
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+// ParseToken parses token and return username
+func ParseToken(tokenString, secret string) (string, error) {
+	token, err := jwt.ParseWithClaims(
+		tokenString,
+		&NamedClaims{},
+		func(token *jwt.Token) (interface{}, error) {
+			return []byte(secret), nil
+		},
+	)
+
+	if claim, ok := token.Claims.(*NamedClaims); ok && token.Valid {
+		return claim.Name, nil
+	}
+
+	return "", err
+}
